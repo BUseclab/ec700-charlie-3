a8e58c0807f877717173af12e98711cd7e37901e
kierk <kierke@bu.edu>
2016-04-13 10:30:36 -0400

    made the naive detect script a bit more robust for testing

--- baremetal/benign_detect.py
+++ baremetal/benign_detect.py
@@ -1,22 +1,34 @@
 #! /usr/bin/env python
 
-import subprocess, sys
+import sys, subprocess
+from subprocess import check_output,CalledProcessError
 
-return1 = subprocess.check_output(
-	"dmesg | grep -i virtual",
-	stderr=subprocess.STDOUT,
-	shell=True)
-#return2 = subprocess.check_output(
-#        "dmidecode | egrep -i 'manufacturer|product'",
-#        stderr=subprocess.STDOUT,
-#        shell=True)
+return1 = None
+return2 = None
+return3 = None
 
-#return3 = subprocess.check_output(
-#        "cat /proc/ide/hd*/model",
-#        stderr=subprocess.STDOUT,
-#        shell=True)
-
+try:
+	return1 = subprocess.check_output(
+		"dmesg | grep -i virtual",
+		stderr=subprocess.STDOUT,
+		shell=True)
+except CalledProcessError as e:
+    return1 = e.output
+try:
+	return2 = subprocess.check_output(
+        	"dmidecode | egrep -i 'manufacturer|product'",
+        	stderr=subprocess.STDOUT,
+        	shell=True)
+except CalledProcessError as e:
+    return2 = e.output
+try:
+	return3 = subprocess.check_output(
+        	"cat /proc/ide/hd*/model",
+	        stderr=subprocess.STDOUT,
+        	shell=True)
+except CalledProcessError as e:
+    return3 = e.output
 
 print return1
-#print return2
-#print return3
+print return2
+print return3
